7da457b4e0e5bd5adbe13e52431af6283518dcc6
BPMinaker <bminaker@uwindsor.ca>
2022-03-16 11:54:25 -0400

    new examples

---
+++ quarter_car_range_damping.jl
@@ -0,0 +1,21 @@
+using EoM
+
+build_examples()
+include(joinpath("examples", "input_ex_quarter_car.jl"))
+
+# here you can enter your vehicle specs by name
+ms = 500
+mu = 50
+kt = 150000
+ks = 18000
+vpts = 500:50:3000
+
+f(x) = input_ex_quarter_car(; ks, ms, mu, kt, cs = x)
+verbose = (vpts .== 500)
+system = f.(vpts)
+output = run_eom!.(system, verbose)
+result = analyze.(output, verbose)
+
+summarize(system, vpts, result, vpt_name = ["c" "Damping" "Ns/m"], ss = [0, 0, 0])
+
+println("Done.")

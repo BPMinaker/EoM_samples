d8d34882c0ae29de71254bb2ead2ea00779005f5
BPMinaker <bminaker@uwindsor.ca>
2022-06-02 17:59:11 -0400

    updates to roll centre model

--- full_car_roll_centre.jl
+++ full_car_roll_centre.jl
@@ -10,10 +10,10 @@ a = 1.5
 b = 1.5
 tf = 1.6
 tr = 1.6
-kf = 18000
-kr = 18000
-cf = 1000
-cr = 1000
+kf = 30000
+kr = 30000
+cf = 2500
+cr = 2500
 krf = 500
 krr = 500
 muf = 20
@@ -22,66 +22,54 @@ hf = 0.2
 hr = 0.2
 hG = 0.5
 
-
 # build system description
 # note that the linear tire cornering stiffness defaults to zero
 # but we have an actuator to apply the nonlinear tire force model
 system = input_full_car_rc(; m, u, a, b, tf, tr, kf, kr, cf, cr, krf, krr, muf, mur, hf, hr, hG, cfy = 0, cry = 0)
-
-# build equation of motion, analyze
 output = run_eom!(system, true)
-# display(getfield.(system.flex_points,:name))
-
 result = analyze(output, true)
 
 # get static tire normal loads (kN)
-Z0 = vcat(getfield.(system.flex_points[[3, 4, 7, 8]],:preload)...)
+# display(getfield.(system.flex_points,:name))
+Z0 = vcat(getfield.(system.flex_points[[1, 2, 5, 6]],:preload)...)
 
 # a smooth step function, 3 degrees, with its midpoint (i.e. 1.5) at t = 2
 # the 0.5 terms change the range from -1...1 to 0...1
 # the 4 determines how quickly the step occurs
 function steer(t)
-    3 * (0.5 * tanh(4 * (t - 2)) + 0.5)
+    3 * (0.5 * tanh(4 * (t - 1.5)) + 0.5)
 end
 
 # assume LF, LR, RF, RR sequence
-function uu(x,t)
-    # get weight transfer
+# compute applied tire force
+function u_in(x,t)
+    # get sensor outputs (D=0)
     y = result.ss_eqns.C * x
     # get total normal load
     Z = Z0 - y[[1, 2, 5, 6]]
     # get slip angles
     slip = y[[3, 4, 7, 8]] .- steer(t) * [1, 0, 1, 0] * pi / 180
-    # call tire force
+    # compute tire force
     tire(Z, slip)
 end
 
-
-#=
-function uu(x,t)
-    [1000, -1000, -1000, 1000]
-end
-=#
-
 # simple nonlinear tire with load sensitivity
 function tire(Z, slip)
-    -(0.94 .- 1.0e-5 .* Z) .* Z .* tanh.(10 .* slip)
+    -(1.2 .- 3.0e-5 .* Z) .* Z .* tanh.(15 .* slip)
 end
 
 println("Solving time history...")
 t = 0:0.002:8
 
-y, xu = splsim(result.ss_eqns, uu, t; flag = true)
+y, xu = splsim(result.ss_eqns, u_in, t; flag = true)
 y = hcat(y...)'
 
 t = t[1:4:end]
 y = y[1:4:end, :]
 YY = xu'[1:4:end, end-3:end]
-
+acc = sum(YY, dims=2)/(m + 2 * muf + 2 * mur)
 ZZ = Z0' .- y[:,[1, 2, 5, 6]]
 slip = y[:, [3, 4, 7, 8]] * 180 / pi - steer.(t) .* [1, 0, 1, 0]' 
-# YY = tire.(ZZ, pi / 180 * slip)
-acc = sum(YY, dims=2)/(m + 2 * muf + 2 * mur)
 
 # set plot text, etc
 lw = 2 # thicker plot lineweight
@@ -118,13 +106,34 @@ ss = zeros(size(result.ss_eqns.D))
 ss[15:16, 1:4] = ones(2, 4)
 bode = ss
 
-summarize(system, result; plots, ss, bode, format = :html)
+# note that the yaw mode shows no eigenvalue because the cornering stiffness is set to zero
+# we know the slope of the tire model
+# so we can add a linear tire model and recompute the eigenvalues
+# note: d tanh ax / dx = asech^2 ax 
+# but sech(0) = 1, so d tanh ax|x=0 = a
+# use the fact that tanh(x->inf) = 1 to compute the normal force term only
+cy = -15 * tire(Z0[1:2], [1e6, 1e6])
+system2 = input_full_car_rc(; m, u, a, b, tf, tr, kf, kr, cf, cr, krf, krr, muf, mur, hf, hr, hG, cfy = cy[1], cry = cy[2])
+output2 = run_eom!(system2, true)
+result2 = analyze(output2, true)
+
+# let's keep the initial steady state and freq resp results
+result2.w = result.w
+result2.freq_resp = result.freq_resp
+result2.mag = result.mag
+result2.phase = result.phase
+result2.ss_resp = result.ss_resp
+
+summarize(system2, result2; plots, ss, bode, format = :html)
 
 # generate animations of the mode shapes
 using EoM_X3D
-animate_modes(system, result)
-
-# using EoM_TeX
-# write_report(system, result, true; bode = copy(ss))
+animate_modes(system2, result2, true)
 
 
+
+
+
+
+
+println("Done.")

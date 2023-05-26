903ab0726ac59de98a2a28c0ac4354a0db2828fe
Bruce Minaker <bminaker@uwindsor.ca>
2023-05-25 22:50:39 -0400

    default params

--- beam.jl
+++ beam.jl
@@ -2,17 +2,22 @@ using EoM
 
 include(joinpath("models", "input_ex_beam.jl"))
 
-EI1 = 100
-EI2 = 3 * EI1
-mpul = 10
-l = 0.5
-n = 2
+h = 2 * 0.0254
+b = 1 * 0.0254
+t = 0.060 * 0.0254
+
+EI1 = 200e9 * (b*h^3 - (b - 2*t) * (h - 2*t)^3) / 12
+EI2 = 200e9 * (b^3*h - (b - 2*t)^3 * (h - 2*t)) / 12
+mpul = 7850 * (b*h - (b - 2*t) * (h - 2*t))
+l = 1
+n = 1
 
 system = input_ex_beam(; EI1, EI2, mpul, l, n)
 output = run_eom!(system, true)
 result = analyze(output, true)
 
 bode = [1 0; 0 1]
+verbose = true
 summarize(system, result, verbose; bode)
 # summarize(system, result, verbose; bode, format = :html)
 

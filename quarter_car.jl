c93541bc2a8fef9591ec6a40e8a94ea810baf18b
BPMinaker <bminaker@uwindsor.ca>
2023-06-29 16:04:53 -0400

    comment update

--- quarter_car.jl
+++ quarter_car.jl
@@ -15,7 +15,7 @@ system = input_ex_quarter_car(; ms, mu, kt, ks, cs)
 output = run_eom!(system, verbose)
 result = analyze(output, verbose)
 
-# here we set the input as a random road where z is a function of distance x; t is a sum of 2000 sin waves with random phase angles, and ampltiude decreasing as wavelength shortens; t will be different each time you run the code; the longest wavelength in the sum is the full length of the road, a default of 100 m; the wavelengths shorten as the sequence 100/2, 100/3, 100/4,..., with the shortest wavelength at 100/2000, or 5 cm; the class here is the road roughness, an integer ranging from 3-9.  A class 3 road is very smooth (on the boundary of ISO classes A and B), where class 9 is extremely rough (boundary of ISO classes G and H); the random road function returns a function handle that gives back `z` as a function of `x`
+# here we set the input as a random road where z is a function of distance x; it is a sum of 2000 sin waves with random phase angles, and ampltiude decreasing as wavelength shortens; it will be different each time you run the code; the longest wavelength in the sum is the full length of the road, a default of 100 m; the wavelengths shorten as the sequence 100/2, 100/3, 100/4,..., with the shortest wavelength at 100/2000, or 5 cm; the class here is the road roughness, an integer ranging from 3-9.  A class 3 road is very smooth (on the boundary of ISO classes A and B), where class 9 is extremely rough (boundary of ISO classes G and H); the random road function returns a function handle that gives back `z` as a function of `x`
 zofx = random_road(class=5)
 
 # but we need to convert to time index, where x=ut; assuming a forward speed of u=10 m/s gives
